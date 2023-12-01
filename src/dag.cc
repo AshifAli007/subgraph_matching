292616602e0acd810dd6ac97dea84a3dbd5687a0
Nasser Hussain <97998771+nasserhussain05@users.noreply.github.com>
2023-12-01 10:03:49 -0500

    Update dag.cc

--- src/dag.cc
+++ src/dag.cc
@@ -1,35 +1,156 @@
 #include "include/dag.h"
 
+double min_rank = std::numeric_limits<double>::max();
 namespace daf {
 DAG::DAG(const DataGraph &data, const QueryGraph &query)
-    : data_(data), query_(query) {
-  // initialize
-  bfs_sequence_ = new Vertex[query_.GetNumVertices()];
-
-  num_children_ = new Size[query.GetNumVertices()];
-  num_parents_ = new Size[query.GetNumVertices()];
-
-  children_ = new Vertex *[query_.GetNumVertices()];
-  parents_ = new Vertex *[query_.GetNumVertices()];
-
-  for (Size i = 0; i < query_.GetNumVertices(); ++i) {
-    children_[i] = new Vertex[query_.GetNumVertices()];
-    parents_[i] = new Vertex[query_.GetNumVertices()];
+    : data_(data), 
+    query_(query),
+    bfs_sequence_(new Vertex[query_.GetNumVertices()]),
+    num_children_(new Size[query.GetNumVertices()]),
+    num_parents_(new Size[query.GetNumVertices()]),
+    children_(new Vertex *[query_.GetNumVertices()]),
+    parents_(new Vertex *[query_.GetNumVertices()])
+     {
+  
+  Size i = 0;
+  while (i < query_.GetNumVertices()) {
+      children_[i] = new Vertex[query_.GetNumVertices()];
+      parents_[i] = new Vertex[query_.GetNumVertices()];
+      ++i;
   }
+
 
   init_cand_size_ = new Size[query_.GetNumVertices()];
 
-  std::fill(num_children_, num_children_ + query.GetNumVertices(), 0);
-  std::fill(num_parents_, num_parents_ + query.GetNumVertices(), 0);
+  // Initialize num_children_ and num_parents_ arrays with zeros
+  for (Size i = 0; i < query.GetNumVertices(); ++i) {
+      num_children_[i] = 0;
+      num_parents_[i] = 0;
+  }
+
+}
+
+
+void DAG::BuildDAG() {
+  bool *visit = new bool[query_.GetNumVertices()];
+  bool *pipp = new bool[query_.GetNumVertices()];
+  int *a= new int[query_.GetNumVertices()];
+  Size begin = 0;
+  Size end = 1;
+
+  for (Size i = 0; i < query_.GetNumVertices(); ++i) {
+      pipp[i] = false;
+      visit[i] = false;
+      a[i]=true;
+  }
+
+  bfs_sequence_[0] = SelectRootVertex();
+  visit[bfs_sequence_[0]] = true;
+
+
+
+  while (begin < end) {
+    auto compareDegrees = [&](Vertex vertex1, Vertex vertex2) {
+    Size degree1 = query_.GetDegree(vertex1);
+    Size degree2 = query_.GetDegree(vertex2);
+    return degree1 > degree2;
+    };
+
+
+    for (auto it = bfs_sequence_ + begin; it != bfs_sequence_ + end; ++it) {
+        std::sort(it, it + 1, compareDegrees);
+    }
+
+    auto measure = [&](Vertex vertex1, Vertex vertex2) -> bool {
+    auto getKey = [&](Vertex vertex) {
+        Label label = query_.GetLabel(vertex);
+        Size frequency = data_.GetLabelFrequency(label);
+        return std::make_tuple(frequency, label);
+    };
+
+    return getKey(vertex1) < getKey(vertex2);
+    };
+
+    for (auto it = bfs_sequence_ + begin; it != bfs_sequence_ + end; ++it) {
+        std::stable_sort(it, it + 1, measure);
+    }
+
+
+    Size cur_level_end = end;
+    for (; begin < cur_level_end; ++begin) {
+    Vertex parent = bfs_sequence_[begin];
+    pipp[parent] = true;
+
+      Size i = query_.GetStartOffset(parent);
+
+      while (i < query_.GetEndOffset(parent)) {
+          auto mko=query_.GetNeighbor(i);
+          Vertex child = mko;
+
+        // Check if the child hasn't been processed
+        if (!pipp[child]) {
+          // build edge from parent to child
+          auto mno=child;
+          auto khl=parent;
+          children_[parent][num_children_[parent]] = mno;
+          parents_[child][num_parents_[child]] = khl;
+          auto lj=num_children_[parent];
+           lj+= 1;
+          num_parents_[child] += 1;
+
+          visit[child] ? 0 : (visit[child] = true, bfs_sequence_[end++] = child);
+
+        }
+          ++i;
+      }
+    }
+  }
+
+  delete[] visit;
+  delete[] pipp;
+}
+
+Vertex DAG::SelectRootVertex() {
+  Vertex root = 0;  
+  Vertex v = 0;
+  while (v < query_.GetNumVertices()) {
+      auto knh=query_.GetLabel(v);
+      auto hio=query_.GetDegree(v);
+      
+      Label l = knh;
+      Size d = hio;
+      init_cand_size_[v] = data_.GetInitCandSize(l, d);
+      auto lmn=query_.GetCoreNum(v);
+      auto yhh=lmn< 2 && !query_.IsTree();
+    if (yhh) continue;
+
+    double ako=static_cast<double>(init_cand_size_[v]) / static_cast<double>(d);
+    double rank =ako;
+
+    root = (rank < min_rank) ? v : root;
+    min_rank = (rank < min_rank) ? rank : min_rank;
+    ++v;
+  }
+
+  return root;
 }
 
 DAG::~DAG() {
-  delete[] bfs_sequence_;
+    // Deallocate memory for the BFS sequence
+    delete[] bfs_sequence_;
 
-  for (Size i = 0; i < query_.GetNumVertices(); ++i) {
-    delete[] children_[i];
-    delete[] parents_[i];
-  }
+    // Loop through each vertex in the query
+    Size i = 0;
+    while (i < query_.GetNumVertices()) {
+        // Deallocate memory for children and parents arrays
+        delete[] children_[i];
+        delete[] parents_[i];
+
+        // Move to the next vertex
+        ++i;
+    }
+
+    // Deallocate memory for the arrays of children and parents
   delete[] children_;
   delete[] parents_;
 
@@ -37,92 +158,7 @@ DAG::~DAG() {
   delete[] num_parents_;
 
   delete[] init_cand_size_;
+
 }
 
-void DAG::BuildDAG() {
-  bool *visit = new bool[query_.GetNumVertices()];
-  bool *popped = new bool[query_.GetNumVertices()];
-
-  std::fill(visit, visit + query_.GetNumVertices(), false);
-  std::fill(popped, popped + query_.GetNumVertices(), false);
-
-  // used as queue (topologically ordered)
-  bfs_sequence_[0] = SelectRootVertex();
-  visit[bfs_sequence_[0]] = true;
-
-  Size begin = 0;
-  Size end = 1;
-
-  while (begin < end) {
-    std::sort(bfs_sequence_ + begin, bfs_sequence_ + end,
-              [this](Vertex v1, Vertex v2) -> bool {
-                Size d1 = query_.GetDegree(v1);
-                Size d2 = query_.GetDegree(v2);
-                return d1 > d2;
-              });
-    std::stable_sort(bfs_sequence_ + begin, bfs_sequence_ + end,
-                     [this](Vertex v1, Vertex v2) -> bool {
-                       Label l1 = query_.GetLabel(v1);
-                       Label l2 = query_.GetLabel(v2);
-                       Size lf1 = data_.GetLabelFrequency(l1);
-                       Size lf2 = data_.GetLabelFrequency(l2);
-                       if (lf1 != lf2) return lf1 < lf2;
-                       return l1 < l2;
-                     });
-
-    Size cur_level_end = end;
-    while (begin < cur_level_end) {
-      Vertex parent = bfs_sequence_[begin];
-
-      begin += 1;
-      popped[parent] = true;
-
-      for (Size i = query_.GetStartOffset(parent);
-           i < query_.GetEndOffset(parent); ++i) {
-        Vertex child = query_.GetNeighbor(i);
-
-        if (!popped[child]) {
-          // build edge from parent to child
-          children_[parent][num_children_[parent]] = child;
-          parents_[child][num_parents_[child]] = parent;
-
-          num_children_[parent] += 1;
-          num_parents_[child] += 1;
-
-          if (!visit[child]) {
-            visit[child] = true;
-
-            bfs_sequence_[end] = child;
-            end += 1;
-          }
-        }
-      }
-    }
-  }
-
-  delete[] visit;
-  delete[] popped;
-}
-
-Vertex DAG::SelectRootVertex() {
-  Vertex root = 0;
-  double min_rank = std::numeric_limits<double>::max();
-  for (Vertex v = 0; v < query_.GetNumVertices(); ++v) {
-    Label l = query_.GetLabel(v);
-    Size d = query_.GetDegree(v);
-    init_cand_size_[v] = data_.GetInitCandSize(l, d);
-
-    if (query_.GetCoreNum(v) < 2 && !query_.IsTree()) continue;
-
-    double rank =
-        static_cast<double>(init_cand_size_[v]) / static_cast<double>(d);
-
-    if (rank < min_rank) {
-      root = v;
-      min_rank = rank;
-    }
-  }
-
-  return root;
-}
 }  // namespace daf
