7e58a139e3300996c731f42ebe4a7d8a68f0604b
Nasser Hussain <97998771+nasserhussain05@users.noreply.github.com>
2023-12-05 19:57:11 -0500

    Final update ordering.cc

--- src/ordering.cc
+++ src/ordering.cc
@@ -3,58 +3,75 @@
 namespace daf {
 Ordering::Ordering(Size num_query_vertices)
     : num_query_vertices_(num_query_vertices) {
-  extendable_queue_ = new Vertex[num_query_vertices_];
+  auto k=num_query_vertices_;
+  extendable_queue_ = new Vertex[k];
   extendable_queue_size_ = 0;
-  weights_ = new Size[num_query_vertices_];
-  std::fill(weights_, weights_ + num_query_vertices_,
-            std::numeric_limits<Size>::max());
-}
+  weights_ = new Size[k];
+  for (Size i = 0; i < num_query_vertices_; ++i) {
+  weights_[i] = std::numeric_limits<Size>::max();
+  }
 
-Ordering::~Ordering() {
-  delete[] extendable_queue_;
-  delete[] weights_;
 }
 
 void Ordering::Insert(Vertex u, Size weight) {
+  auto p=u;
   extendable_queue_[extendable_queue_size_] = u;
   extendable_queue_size_ += 1;
+  p+=1;
   weights_[u] = weight;
 }
 
-void Ordering::UpdateWeight(Vertex u, Size weight) { weights_[u] = weight; }
+void Ordering::UpdateWeight(Vertex u, Size weight){ 
+auto k=weight;
+weights_[u] = k; 
+}
 
 void Ordering::Remove(Vertex u) {
-  for (Size i = 0; i < extendable_queue_size_; ++i) {
+  Size i = 0;
+  while (i < extendable_queue_size_) {
     if (extendable_queue_[i] == u) {
-      weights_[u] = std::numeric_limits<Size>::max();
-      std::swap(extendable_queue_[i],
-                extendable_queue_[extendable_queue_size_ - 1]);
+      auto k=std::numeric_limits<Size>::max();
+      weights_[u] = k;
+      auto po=extendable_queue_[extendable_queue_size_ - 1];
+      std::swap(extendable_queue_[i], po);
       extendable_queue_size_ -= 1;
       return;
     }
+    ++i;
   }
+
 }
 
 bool Ordering::Exists(Vertex u) {
-  return weights_[u] != std::numeric_limits<Size>::max();
+  auto k=std::numeric_limits<Size>::max();
+  return weights_[u] != k;
 }
 
 Vertex Ordering::PopMinWeight() {
   Size popped_idx = 0;
 
-  for (Size i = 1; i < extendable_queue_size_; ++i) {
-    if (weights_[extendable_queue_[i]] <
-        weights_[extendable_queue_[popped_idx]])
-      popped_idx = i;
-  }
+  Size i = 1;
+    while (i < extendable_queue_size_) {
+        auto io=weights_[extendable_queue_[i]];
+        if (io <weights_[extendable_queue_[popped_idx]])
+            popped_idx = i;
+        ++i;
+    }
 
   Vertex popped_vtx = extendable_queue_[popped_idx];
 
   weights_[popped_vtx] = std::numeric_limits<Size>::max();
-  std::swap(extendable_queue_[popped_idx],
-            extendable_queue_[extendable_queue_size_ - 1]);
+  auto ko=popped_idx;
+  auto po=extendable_queue_[extendable_queue_size_ - 1];
+  std::swap(extendable_queue_[ko],po);
   extendable_queue_size_ -= 1;
 
   return popped_vtx;
 }
+
+Ordering::~Ordering() {
+  delete[] weights_;
+  delete[] extendable_queue_;
+}
+
 }  // namespace daf
