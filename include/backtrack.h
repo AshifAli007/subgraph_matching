6b3e8ba3881fdda3b43a470e5bfefcc592883321
sohailuddinsyed <syedsohail601@gmail.com>
2023-12-05 21:56:57 -0500

    Changes to backtrack.h

--- include/backtrack.h
+++ include/backtrack.h
@@ -18,38 +18,40 @@ class Backtrack {
  public:
   Backtrack( const QueryGraph &query,
             const CandidateSpace &cs, const DataGraph &data);
-  ~Backtrack();
-
   Backtrack &operator=(const Backtrack &) = delete;
-  Backtrack(const Backtrack &) = delete;
 
   uint64_t FindMatches(uint64_t limit);
+  Backtrack(const Backtrack &) = delete;
   inline uint64_t GetNumBacktrackCalls() const;
+  ~Backtrack();
 
  private:
+ BacktrackHelper *helpers_;
   const DataGraph &data_;
-  const QueryGraph &query_;
-  const CandidateSpace &cs_;
+  uint64_t num_backtrack_calls_;
+  bool ComputeExtendableForAllNeighbors(Size cs_v_idx, SearchTreeNode *cur_node);
 
+  SearchTreeNode *node_stack_;
   Ordering *extendable_queue_;
   MatchLeaves *match_leaves_;
-  BacktrackHelper *helpers_;
+  void InitializeNodeStack();
 
   Vertex *mapped_query_vtx_;
-  SearchTreeNode *node_stack_;
+  
   SearchTreeNode **mapped_nodes_;
+  void ComputeDynamicAncestor(Vertex u_nbr, Vertex u);
 
   uint64_t num_embeddings_;
-  uint64_t num_backtrack_calls_;
-  Size backtrack_depth_;
-
+  
+  
+  const QueryGraph &query_;
   Vertex GetRootVertex();
-  void InitializeNodeStack();
+  
   void ComputeExtendable(Size u_nbr_idx, Size cs_v_idx, Vertex u, Vertex u_nbr);
-  void ComputeDynamicAncestor(Vertex u_nbr, Vertex u);
-  bool ComputeExtendableForAllNeighbors(Size cs_v_idx, SearchTreeNode *cur_node
-                                        );
+                                        
   void ReleaseNeighbors(SearchTreeNode *cur_node);
+   const CandidateSpace &cs_;
+   Size backtrack_depth_;
 };
 
 inline uint64_t Backtrack::GetNumBacktrackCalls() const {
